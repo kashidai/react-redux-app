7dd0f1a4bef4ccf10b717c875f003ff6d024306d
Dai <d.k.95.82.8192@icloud.com>
2019-03-18 13:56:22 -0700

    add why-jsx

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
 import React, { Component } from 'react';
 
 
+// class App extends Component {
+//   render() {
+//     return <div>Hellow, world!</div>;
+//   }
+// }
+
 class App extends Component {
-  render() {
-    return <div><h1>Hellow, world!</h1></div>;
-  }
+    render() {
+        return React.createElement(
+            "div",
+            null,
+            "Helloe world!!!"
+
+        );
+    }
 }
 
 export default App;
