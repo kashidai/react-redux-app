21789297f205add9a97481e76a167ba51d2b3a7f
Dai <d.k.95.82.8192@icloud.com>
2019-03-18 17:05:58 -0700

    add more-lsx

--- src/App.js
+++ src/App.js
@@ -9,12 +9,11 @@ import React, { Component } from 'react';
 
 class App extends Component {
     render() {
-        return React.createElement(
-            "div",
-            null,
-            "Helloe world!!!"
-
-        );
+        return (<React.Fragment>
+            <label htmlFor="bar">bar</label>
+       <input type="text" onChange={() => {console.log("Yaaaaaaaa!")} }/>
+        </React.Fragment>
+        )
     }
 }
 
